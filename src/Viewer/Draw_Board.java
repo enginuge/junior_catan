89787445478ef26e8e0e13a50a46488480bafd57
JNugentGH <jack.nugent@hotmail.com>
2021-12-05 22:53:07 +0000

    Created Viewer PAckage and Draw board function

---
+++ src/Viewer/Draw_Board.java
@@ -0,0 +1,19 @@
+package Viewer;
+
+import board.Board;
+
+public class Draw_Board
+{
+
+	public void draw_board()
+	{
+		Board board = Board.getInstance();
+		
+		board.islands
+	}
+	
+	public	static void main(String args[])
+	{
+		draw_board();
+	}
+}
