d7ed6a299bc81246eed2fdcbc109728c1a497dd2
JNugentGH <jack.nugent@hotmail.com>
2022-01-08 11:24:53 +0000

    Update Ask_Yes_No.java

--- src/viewer/Ask_Yes_No.java
+++ src/viewer/Ask_Yes_No.java
@@ -1,76 +1,22 @@
 package viewer;
 
-import java.util.ArrayList;
-import java.util.Scanner;
-
 import Player.Player;
 
-public class Ask_Yes_No implements PlayerInput
+public class Ask_Yes_No extends Get_Input
 {
-	private Player player;
-	
 	public Ask_Yes_No(Player p)
 	{
-		player = p;
+		super(p);
+		
+		this.add_option("Yes");
+		this.add_option("yes");
+		this.add_option("Y");
+		this.add_option("y");
+		this.add_option("No");
+		this.add_option("no");
+		this.add_option("N");
+		this.add_option("n");
+		
+		this.set_prompt("Player "+this.player_name()+":\n");
 	}
-	
-	@Override
-	public String prompt() {
-		String message = String.format("Player %s, Yes or No?\n", this.player.get_name());
-		
-		return message;
-	}
-
-	@Override
-	public String scan() {
-		Scanner sc= new Scanner(System.in);    //System.in is a standard input stream  
-
-		ArrayList<String> options = new ArrayList<String>();
-		options.add("Yes");
-		options.add("yes");
-		options.add("Y");
-		options.add("y");
-		options.add("No");
-		options.add("no");
-		options.add("N");
-		options.add("n");
-		
-		String choice = "";
-		
-		boolean valid = false;
-		
-		for(; !valid;)
-		{
-			// Ensure the player picks an actual option.
-			for(choice=""; !options.contains(choice); )
-			{
-//				for(String c:options)
-//					System.out.printf("%s ", c);
-				
-				choice = sc.nextLine();
-				
-			} // end correct color loop.
-			
-			if(options.contains(choice))
-				valid = true;
-			
-			else
-				System.out.printf("%s\n", this.prompt());
-			
-		} // end selection loop.
-		
-//		sc.close(); // Close the scanner.
-
-		return choice;
-
-	}
-
-	@Override
-	public String get_input()
-	{
-		System.out.printf(this.prompt());
-		
-		return this.scan();
-	}
-
 }
